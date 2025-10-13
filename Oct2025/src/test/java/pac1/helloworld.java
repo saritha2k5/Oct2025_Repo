2416e4e4ce18e89d3e680fd96f43cd5b08a8ee27
Saritha <saritha2k5@gmail.com>
2025-10-13 16:48:32 +0530

    lab5 is completed

---
+++ Oct2025/src/test/java/pac1/helloworld.java
@@ -0,0 +1,12 @@
+package pac1;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Selenium");
+
+	}
+
+}
